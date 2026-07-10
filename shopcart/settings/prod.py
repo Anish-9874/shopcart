e7833193f2c20be83ba91b23ce8968cc1ec9c07c
Anish Basnet <anishbasnet323@gmail.com>
2026-07-10 14:00:36 +0545

    manage requirements txt

--- shopcart/settings/prod.py
+++ shopcart/settings/prod.py
@@ -6,3 +6,32 @@ DEBUG = False
 
 
 
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
+
+
+
+
+DATABASES = {
+
+"default": {
+
+"ENGINE": config("PDB_ENGINE"),
+
+"NAME": config("PDB_NAME"),
+
+"USER": config("PDB_USER"),
+
+"PASSWORD": config("PDB_PASSWORD"),
+
+"HOST": config("PDB_HOST"),
+
+"PORT": config("PDB_PORT"),
+
+}
+
+}
+
+
